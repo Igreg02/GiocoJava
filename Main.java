f56a4b81a33e3bf7a1db9887bfa1734cc0c12f1d
Greg <i.gregale@gmail.com>
2023-10-18 16:34:03 +0200

    I

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+import java.awt.Dimension;
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        JFrame Menu = new JFrame("Gioco del threes");
+        Menu.setSize(300, 300);
+        Menu.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        Menu.setVisible(true);
+
+        JButton Bottone1 = new JButton("1");
+        Bottone1.setMaximumSize(new Dimension(10, 10));
+        Menu.add(Bottone1);
+        
+
+        JButton Bottone2 = new JButton("2");
+        Bottone2.setMaximumSize(new Dimension(10, 10));
+        Menu.add(Bottone2);
+
+        // Visualizza il frame
+        Menu.setVisible(true);
+
+
+
 
     }
 }
