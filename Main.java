ccc68dbfbd612707fcb7faf37746261ed670b5ae
Greg <i.gregale@gmail.com>
2023-10-19 12:05:26 +0200

    Aggiunti tasti di selezione

--- Main.java
+++ Main.java
@@ -1,28 +1,51 @@
-import java.awt.Dimension;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
-    public static void main(String[] args) 
-    {
-        JFrame Menu = new JFrame("Gioco del threes");
-        Menu.setSize(300, 300);
-        Menu.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        Menu.setVisible(true);
 
-        JButton Bottone1 = new JButton("1");
-        Bottone1.setMaximumSize(new Dimension(10, 10));
-        Menu.add(Bottone1);
-        
+    private JFrame frame;
+    private JLabel label;
+    private JButton button1;
+    private JButton button2;
 
-        JButton Bottone2 = new JButton("2");
-        Bottone2.setMaximumSize(new Dimension(10, 10));
-        Menu.add(Bottone2);
+    public Main() {
+        frame = new JFrame("Gioco del trees");
+        frame.setSize(400, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Visualizza il frame
-        Menu.setVisible(true);
+        label = new JLabel("Cosa vuoi fare");
+        label.setFont(new Font("Arial", Font.PLAIN, 24));
 
+        button1 = new JButton("Hosta partita");
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText("Premuto bottone 1");
+            }
+        });
 
+        button2 = new JButton("Cerca Partita");
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText("Premuto bottone 2");
+            }
+        });
 
+        // Posiziona i bottoni negli angoli della finestra, senza toccarli
+        button1.setBounds(0, frame.getHeight() - 50, 100, 50);
+        button2.setBounds(frame.getWidth() - 100, 0, 50, 100);
 
+        frame.add(label, BorderLayout.CENTER);
+        frame.add(button1, BorderLayout.NORTH);
+        frame.add(button2, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
     }
 }
